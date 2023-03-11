8b7add0c7907f883ad3772894c26bd934c2a6557
Muhammad Hakimi <hakimi.maula@digital-omnis.com>
2023-03-11 12:12:32 +0800

    add tasks list page

--- lib/tasks.dart
+++ lib/tasks.dart
@@ -14,10 +14,55 @@ class _TasksState extends State<Tasks> {
   Widget build(BuildContext context) {
     TaskList taskList = Provider.of<TaskList>(context);
     return Scaffold(
-      body: const Center(child: Text('Task List Page')),
+      body: ListView.builder(
+        itemCount: taskList.tasks.length,
+        itemBuilder: (BuildContext context, int index) {
+          final task = taskList.tasks[index];
+          return Card(
+            elevation: 2, // box shadow
+            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 16),
+            child: ListTile(
+              title: Text(task['taskName']),
+              subtitle: Text(
+                task['taskDescription'],
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: const CircleAvatar(
+                      backgroundColor: Colors.red,
+                      child: Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      // Do something when the button is pressed
+                    },
+                  ),
+                  IconButton(
+                    icon: const CircleAvatar(
+                      backgroundColor: Colors.green,
+                      child: Icon(
+                        Icons.check,
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      // Do something when the button is pressed
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print(taskList.tasks);
           Navigator.pushNamed(context, '/addtask');
         },
         backgroundColor: Colors.cyan,
